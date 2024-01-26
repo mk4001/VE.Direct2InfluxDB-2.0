33d387a27fa175ece2ceac783eca23c332ce73d4
MK4001 <50479511+mk4001@users.noreply.github.com>
2024-01-26 10:55:44 +0100

    Update connectToWiFi.ino

--- MK_ESP32_MPPT2InfluxDB_Cloud-II/connectToWiFi.ino
+++ MK_ESP32_MPPT2InfluxDB_Cloud-II/connectToWiFi.ino
@@ -1,3 +1,9 @@
+/*
+  © MK 15 Jan 2024
+  Write Victron Energy MPPT fields to INfluxDB on Cloud
+  based on ESP32 Wroom using Uses VeDirectFrameHandler library
+*/
+
 void connectToWiFi() {
   Serial.print("Connecting to WiFi...");
   WiFi.begin(ssid, password);
